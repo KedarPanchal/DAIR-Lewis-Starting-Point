6200ea4472891bef6ac2de8afb5c1443ad5b3308
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-13 22:47:08 -0500

    Plan out input file format

--- src/robot_start_position.cpp
+++ src/robot_start_position.cpp
@@ -16,6 +16,19 @@ std::variant<BURST::geometry::Polygon2D, std::string> read_polygon(std::istream&
     else return string_repr;
 }
 
+
+/** PLANNING COMMENT (DELETE ONCE DONE)
+ * Things to read from standard input:
+ * - The wall space polygon, which can have some or no holes (done, just make some test files for this)
+ * - The parameters for the wallpapering of the wall space, which are, for each layer, pairs of:
+ *   - The length of the segment partitions of each edge (l)
+ *   - The maximum offset between partition start points of adjacent layers (o_max)
+ *
+ * Input file format:
+ * Line 1: The wall space polygon, however CGAL likes that formatted
+ * Line 2: The number of layers (n) followed by pairs of l and o_max for each layer with everything separated by spaces
+ */
+
 int main() {
     // Read wall space from standard input
     auto maybe_wall_space = read_polygon(std::cin);
