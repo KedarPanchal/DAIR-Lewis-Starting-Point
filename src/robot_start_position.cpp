4ec7d6cef6d61f7abf2a34f7de94aaa797e899be
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-14 17:49:46 -0500

    Add comments describing format of holed and non-holed polygons

--- src/robot_start_position.cpp
+++ src/robot_start_position.cpp
@@ -8,7 +8,7 @@
 #include "utilities.hpp"
 
 
-/** PLANNING COMMENT (DELETE ONCE DONE)
+/* PLANNING COMMENT (DELETE ONCE DONE)
  * Things to read from standard input:
  * - The wall space polygon, which can have some or no holes (done, just make some test files for this)
  * - The parameters for the wallpapering of the wall space, which are, for each layer, pairs of:
@@ -19,6 +19,14 @@
  * Line 1: The wall space polygon, however CGAL likes that formatted
  * Line 2: The number of layers (n) followed by pairs of l and o_max for each layer with everything separated by spaces
  * Line 3: The maximum rotational error for the robot (theta_max)
+ */
+
+/* POLYGON INPUT FORMAT
+ * Non-holed polygon: <number of vertices> <x1> <y1> <x2> <y2> ... <xn> <yn>
+ *   - e.g. 4 0 0 10 0 10 10 0 10
+ * Holed polygon: <number of vertices in boundary> <x1> <y1> ... <xn> <yn>  <number of holes> <number of vertices in hole 1> <x1> <y1> ... <xm> <ym>  <number of vertices in hole 2> <x1> <y1> ... <xp> <yp> ...
+ *  - e.g. 4 0 0 10 0 10 10 0 10  2 4 3 3 3 7 7 7 7 3  4 1 1 1 2 2 2 2 1
+ *  - Note the double spaces between the boundary and the holes and between the holes themselves
  */
 
 int main() {
