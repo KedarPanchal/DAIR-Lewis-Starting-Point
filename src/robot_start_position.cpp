cf1b249131489bcaaeba8c3dcc96bd721f58046c
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-14 00:20:23 -0500

    Add parameter parsing code

--- src/robot_start_position.cpp
+++ src/robot_start_position.cpp
@@ -1,19 +1,63 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <variant>
+#include <list>
+#include <utility>
+#include <exception>
+
 #include <BURST/geometry.hpp>
+#include <BURST/numeric.hpp>
+
+#define CONCAT(a, b) a##b
+#define CONCAT_LINE(a, b) CONCAT(a, b)
+
+template <typename F>
+struct defer : F {
+    ~defer() { static_cast<F&>(*this)(); }
+};
+
+#define DEFER(...) do { \
+    auto CONCAT_LINE(_defer_, __LINE__) = ::defer{[&] __VA_ARGS__}; \
+} while(0)
 
 // Reads a polygon from the input stream
 std::variant<BURST::geometry::Polygon2D, std::string> read_polygon(std::istream& in) {
     // Extract polygon as a string
     std::string string_repr;
     std::getline(in, string_repr);
-    std::istringstream is(string_repr);
+    std::istringstream is{string_repr};
     // Attempt to parse the polygon
     BURST::geometry::Polygon2D polygon;
     // If successfully parsed, return the polygon; otherwise, return the original string
     if (is >> polygon) return polygon;
     else return string_repr;
+}
+
+std::list<std::pair<BURST::numeric::fscalar, BURST::numeric::fscalar>> read_wallpapering_parameters(std::istream& in) {
+    // Extract the line of parameters
+    std::string parameter_line;
+    std::getline(in, parameter_line);
+    std::istringstream is{parameter_line};
+
+    // Extract the number of parameter pairs (layers)
+    int layer_count;
+    if (!(is >> layer_count)) return {};
+    
+    std::list<std::pair<BURST::numeric::fscalar, BURST::numeric::fscalar>> parameters;
+    for (size_t i = 0; i < layer_count; ++i) {
+        std::string l_str, o_max_str;
+        if (!(is >> l_str >> o_max_str)) return {};
+        try {
+            BURST::numeric::fscalar l{l_str};
+            BURST::numeric::fscalar o_max{o_max_str};
+            parameters.emplace_back(l, o_max);
+        } catch (const std::exception& e) { // Don't know the exact exception, so just catch all
+            return {};
+        }
+    }
+
+    return parameters;
 }
 
 
@@ -27,15 +71,27 @@ std::variant<BURST::geometry::Polygon2D, std::string> read_polygon(std::istream&
  * Input file format:
  * Line 1: The wall space polygon, however CGAL likes that formatted
  * Line 2: The number of layers (n) followed by pairs of l and o_max for each layer with everything separated by spaces
+ * Line 3: The maximum rotational error for the robot (theta_max)
  */
 
 int main() {
+    // Maintain an error flag so that we can report all errors at once instead of just the first one encountered
+    bool error_flag = false;
+
     // Read wall space from standard input
     auto maybe_wall_space = read_polygon(std::cin);
     if (std::holds_alternative<std::string>(maybe_wall_space)) {
-        std::cerr << "Error: Invalid wall space polygon: " << std::get<std::string>(maybe_wall_space) << std::endl;
-        return 1;
+        DEFER({ std::cerr << "Error: Invalid wall space polygon: " << std::get<std::string>(maybe_wall_space) << std::endl; });
+        error_flag = true;
     }
     auto wall_space = std::get<BURST::geometry::Polygon2D>(maybe_wall_space);
+    auto parameters = read_wallpapering_parameters(std::cin);
+    if (parameters.empty()) {
+        DEFER({ std::cerr << "Error: Invalid wallpapering parameters or none provided" << std::endl; });
+        error_flag = true;
+    }
+    
+    // Return, casuing all deferred error messages to be printed if any errors were encountered
+    if (error_flag) return 1;
 }
 
