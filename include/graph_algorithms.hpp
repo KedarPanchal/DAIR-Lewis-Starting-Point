debe07ef44985eb093bee2bcb2229dce855e73ae
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-26 10:40:29 -0500

    Link to actual cpp files instead of keeping everything in a header

--- include/graph_algorithms.hpp
+++ include/graph_algorithms.hpp
@@ -7,29 +7,6 @@
 
 #define INF -1
 
-inline std::vector<std::vector<int>> floyd_warshall(const Graph& g) {
-    // Create the distance matrix initialized with INF and set the diagonal to 0
-    // Also set neighboring vertices to 1
-    std::vector<std::vector<int>> dist(g.size(), std::vector<int>(g.size(), INF));
-    for (const auto& [u, neighbors] : g)  {
-        dist[u][u] = 0;
-        for (const auto& [v, _] : neighbors) dist[u][v] = 1;
-    }
-
-    // Actually run the algorithm
-    for (size_t k = 0; k < dist.size(); ++k) {
-        for (size_t i = 0; i < dist.size(); ++i) {
-            if (i == k) continue;
-            for (size_t j = 0; j < dist.size(); ++j) {
-                if (j == k || j == i) continue;
-                // Skip if either path is currently unreachable
-                if (dist[i][k] == INF || dist[k][j] == INF) continue;
-                dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
-            }
-        }
-    }
-
-    return dist;
-}
+std::vector<std::vector<int>> floyd_warshall(const Graph& g);
 
 #endif
--- include/utilities.hpp
+++ include/utilities.hpp
@@ -4,60 +4,16 @@
 #include <variant>
 #include <list>
 #include <string>
-#include <sstream>
 #include <utility>
 
 #include "cgal_types.hpp"
 
 // -- UTILITY FUNCTIONS -------------------------------------------------------
 
-// Reads a polygon from the input stream
-inline std::variant<HoledPolygon, std::string> read_polygon(std::istream& in) {
-    // Extract polygon as a string
-    std::string string_repr;
-    std::getline(in, string_repr);
-    std::istringstream is{string_repr};
-    // Attempt to parse the polygon
-    HoledPolygon polygon;
-    // If successfully parsed, return the polygon; otherwise, return the original string
-    if (is >> polygon) return polygon;
-    else return string_repr;
-}
+std::variant<HoledPolygon, std::string> read_polygon(std::istream& in);
 
-inline std::list<std::pair<fscalar, fscalar>> read_wallpapering_parameters(std::istream& in) {
-    // Extract the line of parameters
-    std::string parameter_line;
-    std::getline(in, parameter_line);
-    std::istringstream is{parameter_line};
+std::list<std::pair<fscalar, fscalar>> read_wallpapering_parameters(std::istream& in);
 
-    // Extract the number of parameter pairs (layers)
-    unsigned int layer_count;
-    if (!(is >> layer_count)) return {};
-    
-    std::list<std::pair<fscalar, fscalar>> parameters;
-    for (unsigned int i = 0; i < layer_count; ++i) {
-        std::string l_str, o_max_str;
-        if (!(is >> l_str >> o_max_str)) return {};
-        try {
-            fscalar l{l_str};
-            fscalar o_max{o_max_str};
-            if (l <= 0 || o_max <= 0) return {};
-            parameters.emplace_back(l, o_max);
-        } catch (const std::exception& e) { // Don't know the exact exception, so just catch all
-            return {};
-        }
-    }
-
-    return parameters;
-}
-
-inline std::variant<fscalar, std::string> read_theta_max(std::istream& in) {
-    std::string string_repr;
-    std::getline(in, string_repr);
-    std::istringstream is{string_repr};
-    fscalar theta_max;
-    if (is >> theta_max) return theta_max;
-    else return string_repr;
-}
+std::variant<fscalar, std::string> read_theta_max(std::istream& in);
 
 #endif
