07b8fb70b7189c996bfc968fc05dc532317115e5
KedarPanchal <kedar.t.panchal@outlook.com>
2026-05-26 23:04:05 -0500

    Fix double-definition bug

--- src/graph_algorithms.cpp
+++ src/graph_algorithms.cpp
@@ -7,16 +7,6 @@
 #include "graph_construction.hpp"
 
 // -- HELPER FUNCTIONS --------------------------------------------------------
-struct pair_hash {
-    template <typename T1, typename T2>
-    size_t operator()(const std::pair<T1, T2>& p) const {
-        size_t seed = 0;
-        boost::hash_combine(seed, p.first);
-        boost::hash_combine(seed, p.second);
-        return seed;
-    }
-};
-
 PolygonSet computeCoverage(const Node& source, const Node& target, const Graph& g) {
     for (const auto& [neighbor, _, ccr] : g.at(source)) {
         if (neighbor == target) return ccr;
